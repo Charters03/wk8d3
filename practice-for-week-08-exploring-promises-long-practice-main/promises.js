a0d4135d024b4aadfb49e35a087aa44df52a2bcc
argon <argonglach@gmail.com>
2022-06-02 18:06:05 -0400

    first two

--- practice-for-week-08-exploring-promises-long-practice-main/promises.js
+++ practice-for-week-08-exploring-promises-long-practice-main/promises.js
@@ -1,14 +1,36 @@
 /* ============================== Phase 1 ============================== */
 /* -------------------------- exploring async -------------------------- */
 
-// Your code here
+function num1() {
+    return 1;
+}
+async function num2() {
+    setTimeout(() => {
+    return 2 + 6;
+ }, 5000)
+}
 
+// console.log('num1', num1());
+// console.log('num2', num2());
 
+num2()
+    // .then(data => {return data + 3})
+    // .then(result => console.log(result, 'phase 1'))
 
 /* ============================== Phase 2 ============================== */
 /* -------------------------- exploring await -------------------------- */
 
-// Your code here
+async function awaiting() {
+    const wait = await num2();
+    console.log('anything')
+    console.log("awaiting", wait);
+    console.log('after wait')
+
+
+}
+
+
+awaiting();
 
 
 
